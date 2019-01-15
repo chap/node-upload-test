898ca3ce6d182df08d267c7d39eadf9a97063863
Chap Ambrose <chapambrose@gmail.com>
2019-01-15 12:38:10 -0600

    tweak request handling

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000
+
 var multer = require('multer');
 var upload = multer();
 
-app.post("/herokutest", upload.array(), function (req, res) {
+
+app.post("/herokutest", upload.single('image'), function (req, res, next) {
   res.status(500).send("Here there should be an error with valuable information for the user");
 });
 
